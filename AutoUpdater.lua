c793a4bdde01c236037e855f3444b20e59ba5e1b
Eldinnie <Eldinnie@users.noreply.github.com>
2019-05-14 12:54:09 +0200

    Update AutoUpdater.lua

--- AutoUpdater.lua
+++ AutoUpdater.lua
@@ -6,10 +6,10 @@ function checkForUpdate()
     WebRequest.get('yourgithubfile.json', function(res)
         if (not(res.is_error)) then
             local response = JSON.decode(res.text)
-		if (response[ScriptClass] > ScriptVersion) then
+		if (response[ScriptVersion] > ScriptVersion) then
 			print('New Version ('..response[ScriptClass]..') of '..ScriptClass..' is available!')
 			--install update?
-			installUpdate()
+			installUpdate(response[ScriptVersion])
 		end
         else
             error(res)
@@ -17,8 +17,8 @@ function checkForUpdate()
     end)
 end
 
-function installUpdate()
-	print('[33ff33]Installing Upgrade to '..ScriptClass..'[-]')
+function installUpdate(newVersion)
+	print('[33ff33]Installing Upgrade to '..ScriptClass..'['..tostring(newVersion)..']')
 	WebRequest.get('yourgithubfile.lua', function(res)
         if (not(res.is_error)) then
 		self.setLuaScript(res.text)
