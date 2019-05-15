44db9630cdcb00b3a2e3c45d0f3c488ee66329e1
MoreThanTom <tomsputz@outlook.com>
2019-05-15 14:07:21 +0100

    Rewrite for readability

--- TTS-Patches.lua
+++ TTS-Patches.lua
@@ -1,34 +1,26 @@
--- Avoid emitting non-standard error
-do
-    local remove = table.remove
-    function table.remove(tab, pos)
-        if #tab < pos then return end
-        return remove(tab, pos)
-    end
+local remove = table.remove
+local pack = table.pack
+local unpack = table.unpack
+local print = print
+-- Avoid crashing when too many calls to print are made
+local count = 0
+Wait.time(function() count = 0 end, 0, -1)
+function print(...)
+    -- The maximum is probably memory dependent, testing might lower it
+    if count == 900 then error('Too many calls to print in frame') end
+    count = count + 1
+    return print(...)
 end
-do
-    local pack = table.pack
-    function table.pack(...)
-        local values = pack(...)
-        -- A patch for this bug (Remove it if the bug is fixed) => (function(...) return table.pack(...) end)().n == 1
-        if values.n == 1 and type(values[1]) == 'nil' then values.n = 0 end
-        return values
-    end
+-- Avoid emitting non-standard error
+function table.remove(tab, pos)
+    if #tab < pos then return end
+    return remove(tab, pos)
+end
+-- A patch for this bug (Remove it if the bug is fixed) => (function(...) return table.pack(...) end)().n == 1
+function table.pack(...)
+    local values = pack(...)
+    if values.n == 1 and type(values[1]) == 'nil' then values.n = 0 end
+    return values
 end
 -- Process the n property for parameters containing nil
-do
-    local unpack = table.unpack
-    function table.unpack(table) return unpack(table, nil, table.n) end
-end
--- Avoid crashing when too many calls to print are made
-do
-  local print = print
-  local count = 0
-  Wait.time(function() count = 0 end, 0, -1)
-  function print(...)
-      -- The maximum is probably memory dependent, testing might lower it
-      if count == 900 then error('Too many calls to print in frame') end
-      count = count + 1
-      return print(...)
-  end
-end
+function table.unpack(table) return unpack(table, nil, table.n) end
